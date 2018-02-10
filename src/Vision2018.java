b1054da46124073005cbf7af6bb16120f5d2690d
BCR <BCR@rbusd.edu>
2018-02-10 15:29:28 -0800

    Network Table Code
    Added code to create a Network table for the Pi.  Directions for
    compiling and deploying code to the Pi were added as well

--- src/Vision2018.java
+++ src/Vision2018.java
@@ -8,19 +8,32 @@ import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.VideoMode;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTable; 
+import edu.wpi.first.networktables.NetworkTableEntry; 
 
 public class Vision2018 {
 		
 	public static GripPipeline pipeline = new GripPipeline();
-	public static double centerX;
+	public static double centerX, centerY;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("test");
 		
+		// Creates a NetworkTable for Raspberry Pi information 
+		
+		NetworkTableEntry xCoord; 
+		NetworkTableEntry yCoord; 	
+		
 		NetworkTableInstance inst = NetworkTableInstance.getDefault();
+		NetworkTable pi = inst.getTable("Pi"); 
 		inst.startClientTeam(294);
+		
+		xCoord = pi.getEntry("X"); 
+		yCoord = pi.getEntry("Y"); 
+		
+		
 		
 		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
 	    camera.setVideoMode(VideoMode.PixelFormat.kYUYV, GripPipeline.IMG_WIDTH, GripPipeline.IMG_HEIGHT, 30);
@@ -45,17 +58,28 @@ public class Vision2018 {
 		    } else {
 		    	// No errors, process the image
 		    	System.out.println("Success!");
-		
+
+		    	// Run the GRIP pipeline
 		    	pipeline.process(m_image);
-//		    	if (!pipeline.filterContoursOutput().isEmpty()) {
-//		            Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
-//	                centerX = r.x + (r.width / 2);
-//	    		    System.out.println("Center of 1st countour:  X = " + centerX);
-//		        } else {
-//		        	System.out.println("No countours found.");
-//		        }
+		    	
+		    	// Get the center X and Y coords of the first contour found
+		    	if (!pipeline.filterContoursOutput().isEmpty()) {
+		            Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
+	                centerX = r.x + (r.width / 2);
+	                centerY = r.y + (r.height / 2);
+
+			    	xCoord.setDouble(centerX); 
+			    	yCoord.setDouble(centerY);
+	                
+	    		    System.out.println("Center of 1st countour:  X = " + centerX + ", Y = " + centerY);
+		        } else {
+		        	System.out.println("No countours found.");
+		        }
+
 		    }
+		    
 	    }
+	  
 	    
 
 	    
